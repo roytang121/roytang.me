c3abc1d3fc20c3b452c0562b872fe7557b358221
Roy Tang <me@roytang.me>
2016-11-04 15:18:37 +0800

    3ds

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('*', function(req, res, next) {
+router.get('/', function(req, res, next) {
   console.log("counter=" + res.counter);
   res.render('index', {
     title: "roytang.me",
